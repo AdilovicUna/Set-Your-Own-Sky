d429d0969cd627a0fb2c6e20caa268ea89401687
Una Adilović <adilovicuna@gmail.com>
2022-09-16 21:13:42 +0200

    upload

---
+++ Scenes/Ground.gd
@@ -0,0 +1,6 @@
+extends Spatial
+
+func update_pos(player_Xpos):
+	if player_Xpos > translation.x:
+		translation.x += 50 # move ground ahead
+		print(translation.x)
---
+++ Scenes/Player.gd
@@ -0,0 +1,10 @@
+extends KinematicBody
+
+onready var ground = get_node_or_null("../Ground")
+const SPEED = 15.0
+
+func _physics_process(delta):
+	var velocity = Vector3.RIGHT * SPEED
+	move_and_slide(velocity)
+	ground.update_pos(translation.x)
+	
