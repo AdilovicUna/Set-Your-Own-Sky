a88f3a94e47b04d4b6cafbb8bb8389c71f637741
Una Adilović <adilovicuna@gmail.com>
2022-09-17 21:35:14 +0200

    hehe

--- Game/Scripts/Ground.gd
+++ Game/Scripts/Ground.gd
@@ -1,10 +1,5 @@
 extends Spatial
 
 func update_pos(player_Xpos):
-<<<<<<< HEAD
     if player_Xpos > translation.x:
         translation.x += 50 # move ground ahead
-=======
-	if player_Xpos > translation.x - 50:
-		translation.x += 50 # move ground ahead
->>>>>>> 788a366cbb8c47718fecdbaf07cb2ea09cc7dd0b
--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -3,8 +3,8 @@ extends KinematicBody
 onready var ground = get_node_or_null("../Environment/Ground")
 const SPEED = 15.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
     var velocity = Vector3.RIGHT * SPEED
-    move_and_slide(velocity)
+    var _return = move_and_slide(velocity)
     ground.update_pos(translation.x)
     
