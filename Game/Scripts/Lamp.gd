e1cf57f474b8af502dedeb2b85c1025de64a7a0c
Azamat Zarlykov <azarlykovcz@gmail.com>
2022-09-17 16:16:19 +0200

    trees adding

--- Game/Scripts/Lamp.gd
+++ Game/Scripts/Lamp.gd
@@ -1,22 +1,19 @@
 extends Spatial
 
 var scene = preload("res://Scenes/Lamp.tscn")
+var next_lamp_x_pos = 0
+var draw_left = true
+const DISTANCE_BETWEEN_LAMPS = 40
 
 onready var player = get_node_or_null("../../Player")
 
-const DISTANCE_BETWEEN_LAMPS = 40
-
-var next_lamp_x_pos = 0
-var draw_left = true
-
 func _ready():
-	# generate biginning
+	# generate beginning
 	for i in range (0,5):
 		add_lamp()
 
 func _physics_process(_delta):
 	if player.translation.x > next_lamp_x_pos - DISTANCE_BETWEEN_LAMPS:
-		print("ADDING")
 		add_lamp()
 		
 	delete_lamp()
@@ -24,10 +21,8 @@ func _physics_process(_delta):
 func delete_lamp():
 	for lamp in get_children():
 		if player.translation.x > lamp.translation.x + DISTANCE_BETWEEN_LAMPS:
-			print("DELETED")
 			lamp.queue_free()
 			
-	
 func add_lamp():
 	var lamp = scene.instance()
 	next_lamp_x_pos += DISTANCE_BETWEEN_LAMPS
--- Game/Scripts/Trees.gd
+++ Game/Scripts/Trees.gd
@@ -1,9 +1,24 @@
 extends Spatial
 
 var scene = preload("res://Scenes/Tree.tscn")
+var rand = RandomNumberGenerator.new()
 
-# o
+onready var player = get_node_or_null("../../Player")
+
 func _ready():
-	pass
+	#rand.randomize()
+	#var num_trees = rand.randi_range(30, 50)
+	var trees = 10
+	
+	for n_tree in trees:
+		# road size width and lendth is 9 and 100 and position (0,0.1,0)
+		var x = rand.randi_range(0, 100)
+		
+		var y = randi.range()
 func _process(delta):
 	pass
+
+func add_tree():
+	var tree = scene.instance()
+
+	add_child(tree)
